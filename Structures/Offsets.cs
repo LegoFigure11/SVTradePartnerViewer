263fea1b000509221f2e8246e161a4d629152bad
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2023-03-18 12:35:02 +1100

    Update offsets for SV 1.2.0
    Closes #1

--- Structures/Offsets.cs
+++ Structures/Offsets.cs
@@ -5,10 +5,10 @@
         public const string ScarletID = "0100A3D008C5C000";
         public const string VioletID = "01008F6008C5E000";
 
-        public static IReadOnlyList<long> MyStatusPointer { get; } = new long[] { 0x4384B18, 0x148, 0x40 };
-        public static IReadOnlyList<long> Trader1MyStatusPointer { get; } = new long[] { 0x437ECE0, 0x48, 0xB0, 0x0 };
-        public static IReadOnlyList<long> Trader2MyStatusPointer { get; } = new long[] { 0x437ECE0, 0x48, 0xE0, 0x0 };
+        public static IReadOnlyList<long> MyStatusPointer { get; } = new long[] { 0x44A98C8, 0x100, 0x40 };
+        public static IReadOnlyList<long> Trader1MyStatusPointer { get; } = new long[] { 0x44A3528, 0x48, 0xB0, 0x0 };
+        public static IReadOnlyList<long> Trader2MyStatusPointer { get; } = new long[] { 0x44A3528, 0x48, 0xE0, 0x0 };
 
-        public static IReadOnlyList<long> LinkTradePartnerNIDPointer { get; } = new long[] { 0x43A28F0, 0xF8, 0x8 };
+        public static IReadOnlyList<long> LinkTradePartnerNIDPointer { get; } = new long[] { 0x44C7730, 0xF8, 0x8 };
     }
 }
